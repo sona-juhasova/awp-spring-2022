3b772aac33929353fa3c4d9efd7c852423163851
Sona Juhasova <soj@web2media.dk>
2022-03-23 12:29:09 +0100

    books

--- lesson-08/remix-mongoose/app/routes/books/$bookId.jsx
+++ lesson-08/remix-mongoose/app/routes/books/$bookId.jsx
@@ -1,10 +1,10 @@
 
-import { useLoaderData, json } from "remix";
+import { useLoaderData, json, useCatch} from "remix";
 import connectDb from "~/db/connectDb.server.js";
 
 export async function loader({ params }) {
   const db = await connectDb();
-  const book = db.models.Book.findById(params.bookId);
+  const book = await db.models.Book.findById(params.bookId);
   if (!book) {
     throw new Response(`Couldn't find book with id ${params.bookId}`, {
       status: 404,
@@ -23,5 +23,21 @@ export default function BookPage() {
         <pre>{JSON.stringify(book, null, 2)}</pre>
       </code>
     </div>
+  );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+  return(
+    <div>
+      <h2>{caught.data}</h2>
+    </div>
+  );
+}
+
+export function ErrorBoundary({ error }) {
+  return (
+    <h1>{error.name} : {error.message}</h1>
+  
   );
 }
