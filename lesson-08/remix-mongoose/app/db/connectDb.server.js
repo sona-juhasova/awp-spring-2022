8f5a887fb776c00349e3f6059648e70fda64bf80
Dan Okkels Brendstrup <dob@eaaa.dk>
2022-03-24 09:48:21 +0100

    L8: Recreate models in dev to pick up schema edits

--- lesson-08/remix-mongoose/app/db/connectDb.server.js
+++ lesson-08/remix-mongoose/app/db/connectDb.server.js
@@ -15,26 +15,35 @@ if (!MONGODB_URL) {
   }
 }
 
-// Log events
-mongoose.connection.on("connected", () => {
-  console.log("Mongoose connected, NODE_ENV=%s", NODE_ENV);
-});
-
-mongoose.connection.on("disconnected", () => {
-  console.log("Mongoose DISCONNECTED, NODE_ENV=%s", NODE_ENV);
-});
-
 // We reuse any existing Mongoose db connection to avoid creating multiple
 // connections in dev mode when Remix "purges the require cache" when reloading
 // on file changes.
 export default async function connectDb() {
   // Reuse the existing Mongoose connection if we have one...
+  // https://mongoosejs.com/docs/api/connection.html#connection_Connection-readyState
   if (mongoose.connection.readyState > 0) {
+    // ...but overwrite all models in development to ensure we pick up any changes made in schemas
+    if (NODE_ENV === "development") {
+      for (const model of models) {
+        mongoose.connection.deleteModel(model.name);
+        mongoose.connection.model(model.name, model.schema, model.collection);
+      }
+    }
+
     return mongoose.connection;
   }
 
-  // ...or create a new connection:
-  const conn = await mongoose.connect(MONGODB_URL, {
+  // If no connection exists yet, set up event logging...
+  mongoose.connection.on("connected", () => {
+    console.log("Mongoose connected, NODE_ENV=%s", NODE_ENV);
+  });
+
+  mongoose.connection.on("disconnected", () => {
+    console.log("Mongoose DISCONNECTED, NODE_ENV=%s", NODE_ENV);
+  });
+
+  // ...and create a new connection:
+  await mongoose.connect(MONGODB_URL, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
   });
@@ -43,8 +52,8 @@ export default async function connectDb() {
   // https://mongoosejs.com/docs/connections.html#multiple_connections
   // So we set them up here to avoid overwriting and getting errors in dev mode.
   for (const model of models) {
-    conn.model(model.name, model.schema, model.collection);
+    mongoose.connection.model(model.name, model.schema, model.collection);
   }
 
-  return conn;
+  return mongoose.connection;
 }
